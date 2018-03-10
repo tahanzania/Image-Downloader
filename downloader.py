4b557ec4ba20bd1076995534a88dc5faaa753734
Taha Dharamsi <30271769+tahanzania@users.noreply.github.com>
2018-03-10 00:27:28 -0500

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,17 +1,14 @@
+#This allows for auto-downloading images from a link
+#Importing Libraries
 import random
-
 import urllib
 
-
-
 def download_image(url):
-
+    
+    #Names the file and converts to PNG
     img_name = random.randrange(100, 500)
-
     full_name = str(img_name) + '.png'
 
     urllib.urlretrieve(url,full_name)
-
-
 
 download_image('http://www.tahadharamsi.com/uploads/1/2/9/4/1294209/4920220_orig.jpg')
